3867dd19c3edd221953a43f3cec86622876b7a49
ptrwrbl <pirrowsky@gmail.com>
2022-02-24 13:11:08 +0100

    🤔

--- src/components/Compatibility/CompatibilityListWrapper.js
+++ src/components/Compatibility/CompatibilityListWrapper.js
@@ -6,7 +6,7 @@ function CompatibilityListWrapper(props) {
         if (props.errors)
             return (<Alert variant="danger">{props.errors.code ? `[${props.errors.code}] ${props.errors.message}` : `${props.errors.message}`}</Alert>);
 
-        return (<p className="mt-5 h2 text-center text-muted">No devices found. Try add more devices using button above.</p>)
+        return (<p className="mt-5 h2 text-center text-muted">No devices found.</p>)
     }
 
     return (
